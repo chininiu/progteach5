9e33f018c05e754f9d21c7665e7f5b95332a8ff9
AXIOMS <159263901+chininiu@users.noreply.github.com>
2024-12-07 09:44:29 +0800

    Update Login_js.js

--- LOGIN_PAGE/Login_js.js
+++ LOGIN_PAGE/Login_js.js
@@ -108,7 +108,7 @@ $(document).ready(function()
     })
 
     $('#back').click(function(){
-        window.location.href ="../HOME_PAGE (Not Logged)/index.html"
+        window.location.href ="/index.html"
     })
 
     
